da6842ded630f149fbe16fa140b79bb6d4805bf8
Anıl Alkan <99561667+anil-alc@users.noreply.github.com>
2022-02-19 22:18:17 +0300

    Add files via upload

---
+++ 3.8_regresyonGrafik.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  9 23:59:23 2021
+
+@author: Anıl Alkan
+"""
+
+
+#1.kutuphaneler
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#2.veri onisleme
+#2.1.veri yukleme
+veriler = pd.read_csv('satislar.csv')
+#pd.read_csv("veriler.csv")
+#test
+print(veriler)
+
+aylar= veriler[['Aylar']]
+print(aylar)
+
+satislar= veriler[['Satislar']]
+print(satislar)
+
+satislar2= veriler.iloc[:,:1].values
+print(satislar2)
+'''
+x = veriler.iloc[:,1:4].values #bağımsız değişkenler
+y = veriler.iloc[:,4:].values #bağımlı değişken
+print(y)
+'''
+#verilerin egitim ve test icin bolunmesi
+from sklearn.model_selection import train_test_split
+
+x_train, x_test,y_train,y_test = train_test_split(aylar,satislar,test_size=0.33, random_state=0)
+'''
+#verilerin olceklenmesi
+from sklearn.preprocessing import StandardScaler
+
+sc=StandardScaler()
+
+X_train = sc.fit_transform(x_train)
+X_test = sc.transform(x_test)
+
+'''
+
+#from sklearn.linear_model import LogisticRegression
+#logr = LogisticRegression(random_state=0)
+#logr.fit(X_train,y_train)
+
+#y_pred = logr.predict(X_test)
+#print(y_pred)
+#print(y_test)
+'''
+Y_train= sc.fit_transform(y_train)
+Y_test= sc.fit_transform(y_test)
+'''
+#model inşası(linear reggresion) 
+from sklearn.linear_model import LinearRegression
+lr=LinearRegression()
+lr.fit(x_train,y_train)
+
+tahmin= lr.predict(x_test)
+
+x_train= x_train.sort_index()
+y_train= y_train.sort_index()
+
+plt.plot(x_train,y_train)
+plt.plot(x_test,lr.predict(x_test))
+
+plt.title("Aylara göre satış")
+plt.xlabel("Aylar")
+plt.ylabel("Satışlar")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
