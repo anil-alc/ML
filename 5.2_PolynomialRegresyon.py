2bbff45688213f12a11dbb8554b06de5da12432d
Anıl Alkan <99561667+anil-alc@users.noreply.github.com>
2022-02-22 17:09:42 +0300

    Add files via upload

---
+++ 5.2_PolynomialRegresyon.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  8 21:45:05 2021
+
+@author: Anıl Alkan
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#veri yükleme 
+
+veriler= pd.read_csv("https://bilkav.com/maaslar.csv")
+
+#dataframe dilimleme
+x= veriler.iloc[:,1:2]
+y= veriler.iloc[:,2:]
+X= x.values
+Y= y.values
+
+#lineer regresyon
+#doğrusal model oluşturma
+from sklearn.linear_model import LinearRegression
+lin_reg= LinearRegression()
+lin_reg.fit(X,Y)
+
+plt.scatter(X,Y, color="red")
+plt.plot(x,lin_reg.predict(X), color="blue")
+plt.show()
+
+#polynomial regression
+#doğrusal olmayan model oluşturma
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg= PolynomialFeatures(degree=4)
+x_poly= poly_reg.fit_transform(X)
+print(x_poly)
+lin_reg2= LinearRegression()
+lin_reg2.fit(x_poly,y)
+#görselleştirme
+plt.scatter(X,Y, color="red")
+plt.plot(X,lin_reg2.predict(poly_reg.fit_transform(X)))
+plt.show()
+
+
+#tahminler
+
+print(lin_reg.predict([[11]]))
+print(lin_reg.predict([[6.6]]))
+
+print(lin_reg2.predict(poly_reg.fit_transform([[6.6]])))
+print(lin_reg2.predict(poly_reg.fit_transform([[11]])))
